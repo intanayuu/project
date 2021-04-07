a78e0ab0e0b8d8716ff994040c30099df316a459
intanayuu <58931667+intanayuu@users.noreply.github.com>
2021-04-07 13:45:00 +0800

    Add files via upload

---
+++ database/migrations/2021_04_06_034135_test.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Test extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $table->id();
+        $table->string('username')->unique();
+        $table->string('password');
+        $table->timestamps();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
